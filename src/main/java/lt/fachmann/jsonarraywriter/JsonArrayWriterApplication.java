78f64ac31dcb882f07ae6bc213e41e316af4ba7c
Bartłmoiej Wierciński <bmwbartek1@gmail.com>
2019-03-09 16:03:02 +0100

    init commit

---
+++ src/main/java/lt/fachmann/jsonarraywriter/JsonArrayWriterApplication.java
@@ -0,0 +1,13 @@
+package lt.fachmann.jsonarraywriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonArrayWriterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonArrayWriterApplication.class, args);
+    }
+
+}
